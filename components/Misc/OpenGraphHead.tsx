6c23b7ddf5de2f5d35794716cef09b4fd5440e78
vivekthube <119954879+vivekthube@users.noreply.github.com>
2024-04-22 17:18:51 +0530

    Update OpenGraphHead.tsx

--- components/Misc/OpenGraphHead.tsx
+++ components/Misc/OpenGraphHead.tsx
@@ -4,7 +4,7 @@ const OpenGraphHead = () => (
     <title>Vivek Thube | Software Engineer</title>
     <meta name="description" content="My personal solace place on web-earth." />
     <meta property="og:title" content="Vivek Thube | Software Engineer" />
-    <meta property="og:site_name" content="Marc Jhon Lawingco" />
+    <meta property="og:site_name" content="Vivek thube" />
     <meta
       property="og:description"
       content="Ohh you found me?. Howdy! I am Vivek Thube. Or you could call me VT. That works too . . . I am a Software Engineer"
